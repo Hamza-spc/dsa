f1f371d8954ac6850cb10f7c1a4f4068fc4bd1a5
Hamza <hamza.2simou@gmail.com>
2026-04-19 16:10:27 +0100

    contains duplicate done

--- src/main/java/org/com/arrays/ContainsDuplicate.java
+++ src/main/java/org/com/arrays/ContainsDuplicate.java
@@ -1,4 +1,22 @@
 package org.com.arrays;
 
+
+import java.util.HashSet;
+
 public class ContainsDuplicate {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,1};
+        HashSet <Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])){
+                System.out.println("duplicate found : " + nums[i]);
+                return;
+            }
+            set.add(nums[i]);
+        }
+        System.out.println("no duplicate");
+
+
+    }
 }
